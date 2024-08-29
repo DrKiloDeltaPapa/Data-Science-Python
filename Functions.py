52160c0c6088c7f285260122fcde022feae749c7
DrKiloDeltaPapa <dr.kareemdavidpiper@gmail.com>
2024-08-28 20:24:54 -0400

    new

---
+++ Functions.py
@@ -0,0 +1,47 @@
+## Case Match Conditional for Shapes
+
+
+# def determine_shape(sides):
+#     match sides:
+#         case 3:
+#             return "Triangle"
+#         case 4:
+#             return "Quadrilateral"
+#         case 5:
+#             return "Pentagon"
+#         case _:
+#             return f"Polygon with {sides} sides"
+
+
+# ## Example usage:
+
+# shape = determine_shape(6)
+# print(shape)
+
+
+## Another Example of the  Match Case or Case Match  Condtional
+
+# value = "four"
+# match value:
+#     case "one":
+#         result = 1
+#     case "two":
+#         result = 2
+#     case "three":
+#         result = 3
+#     case "four" | "five":
+#         result = (4, 5)
+#     case _:
+#         result = -1
+
+# print(result)
+
+
+### Collinearity Check Function
+## Returns True if vectors starting from the origin are collinear
+def collinearity(x1, y1, x2, y2):
+    return x1 * y2 == x2 * y1
+
+
+coltest = collinearity(2, 1, 1, 2)
+print(coltest)
